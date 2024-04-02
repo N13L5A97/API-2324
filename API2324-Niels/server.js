21971476aa61e46a73f134024b5a2cc0a56d8b84
Niels Aling <niels.aling@hva.nl>
2024-04-02 14:34:17 +0200

    set up server.js and get movies from marvel

--- API2324-Niels/server.js
+++ API2324-Niels/server.js
@@ -1,17 +1,55 @@
 require("dotenv").config();
 const express = require("express");
+const router = express.Router();
 const app = express();
-const port = process.env.PORT;
+
+const PORT = process.env.PORT;
+const API_URL = process.env.API_URL;
+const API_TOKEN = process.env.API_TOKEN;
+
+const url = `${API_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_companies=7505'`;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(router);
 
 // home
-app.get("/", async function (req, res) {
-//   res.render("pages/index");
-  res.send("Hello World");
+app.get("/", function (req, res) {
+  try{
+    const movies = getMovies();
+
+    res.render("pages/index", { movies });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.get("/about", function (req, res, next) {
+  res.render("pages/about");
 });
+
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_TOKEN}`
+  }
+};
+
+const getMovies = async () => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json.results);
+    return json.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
