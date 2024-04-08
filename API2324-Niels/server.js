846e68c3989cf5298039a72ff393cbf37bafa5fb
Niels Aling <niels.aling@hva.nl>
2024-04-08 14:38:46 +0200

    Add navbar styles and update homepage layout

--- API2324-Niels/server.js
+++ API2324-Niels/server.js
@@ -7,32 +7,30 @@ const PORT = process.env.PORT;
 const API_URL = process.env.API_URL;
 const API_TOKEN = process.env.API_TOKEN;
 
-const url = `${API_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_companies=7505'`;
+const url = `${API_URL}/discover/movie?include_adult=true&include_video=true&language=en-US&sort_by=popularity.desc&with_companies=420`;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(router);
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+    console.log("Server listening on PORT", PORT); 
+  });
 
 // home
-app.get("/", function (req, res) {
+app.get("/", async (req, res) => {
   try{
-    const movies = getMovies();
+    const movies = await getMovies();
+    console.log(movies);
 
-    res.render("pages/index", { movies });
+    res.render('pages/index', {
+      movies: movies
+    });
 
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
-});
-
-app.get("/about", function (req, res, next) {
-  res.render("pages/about");
-});
-
-app.listen(PORT, function (err) {
-  if (err) console.log(err);
-  console.log("Server listening on PORT", PORT);
 });
 
 const options = {
@@ -47,7 +45,8 @@ const getMovies = async () => {
   try {
     const response = await fetch(url, options);
     const json = await response.json();
-    console.log(json.results);
+
+    // console.log(json.results);
     return json.results;
   } catch (error) {
     console.error(error);
