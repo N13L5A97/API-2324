54602f9a4d02bd7a8ae6394f03d6a13d49239f03
Niels Aling <niels.aling@hva.nl>
2024-04-09 10:43:40 +0200

    Add new pages for Tom Holland, Andrew Garfield, and Tobey Maguire

--- API2324-Niels/server.js
+++ API2324-Niels/server.js
@@ -7,7 +7,16 @@ const PORT = process.env.PORT;
 const API_URL = process.env.API_URL;
 const API_TOKEN = process.env.API_TOKEN;
 
-const url = `${API_URL}/discover/movie?include_adult=true&include_video=true&language=en-US&sort_by=popularity.desc&with_companies=420`;
+const oldurl = `${API_URL}/discover/movie?include_adult=true&include_video=true&language=en-US&sort_by=popularity.desc&with_companies=420`;
+const url = `${API_URL}/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=primary_release_date.desc&with_cast=1136406%7C2219%7C37625&with_companies=420%7C7505%7C19551&with_original_language=en`;
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_TOKEN}`
+  }
+};
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -17,8 +26,45 @@ app.listen(PORT, function (err) {
     console.log("Server listening on PORT", PORT); 
   });
 
-// home
+// index
 app.get("/", async (req, res) => {
+  try{
+    res.render('pages/index');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/tom-holland", async (req, res) => {
+  try{
+    res.render('pages/tom-holland');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/andrew-garfield", async (req, res) => {
+  try{
+    res.render('pages/andrew-garfield');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/tobey-maguire", async (req, res) => {
+  try{
+    res.render('pages/tobey-maguire');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// old index
+app.get("/old", async (req, res) => {
   try{
     const movies = await getSpiderManMovies();
     console.log(movies);
@@ -33,30 +79,10 @@ app.get("/", async (req, res) => {
   }
 });
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_TOKEN}`
-  }
-};
-
-// const getMarvelMovies = async () => {
-//   try {
-//     const response = await fetch(url, options);
-//     const json = await response.json();
-
-//     console.log(json.results);
-//     return json.results;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
 // get spiderman movies
 const getSpiderManMovies = async () => {
   try {
-    const response = await fetch(`${API_URL}/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=primary_release_date.desc&with_cast=1136406%7C2219%7C37625&with_companies=420%7C7505%7C19551&with_original_language=en`, options);
+    const response = await fetch(url, options);
     const json = await response.json();
     console.log(json.results);
     return json.results;
@@ -90,3 +116,15 @@ getSpiderManMovies();
 //marvel entertainment: 7505
 //marvel studios: 420
 //marvel enterprises: 19551
+
+// const getMarvelMovies = async () => {
+//   try {
+//     const response = await fetch(url, options);
+//     const json = await response.json();
+
+//     console.log(json.results);
+//     return json.results;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
